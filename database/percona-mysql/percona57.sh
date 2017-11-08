8fe8a1dfa21e649b7c1bf2394a99af8dd80baeec
Ardy Satria Hasanuddin <ardy.hasanuddin@indodeveloper.com>
2017-11-08 14:07:07 +0700

    percona mysql 57 installation scripts

---
+++ database/percona-mysql/percona57.sh
@@ -0,0 +1,6 @@
+#bin/bash
+
+wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb
+dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb
+sudo apt-get update
+sudo apt-get install percona-server-server-5.7
