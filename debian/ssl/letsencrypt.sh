301badbc7864f33190f80b3cc3d974352c2ffd5b
Ardy Satria Hasanuddin <ardy.hasanuddin@indodeveloper.com>
2017-11-08 16:11:36 +0700

    script install letsencrypt debian/ubuntu

---
+++ debian/ssl/letsencrypt.sh
@@ -0,0 +1,27 @@
+#bin/bash
+
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install python-certbot-nginx -y
+
+# Step 2 — Setting up Nginx
+
+# Step 3 — Allowing HTTPS Through the Firewall
+echo "*************************************************"
+echo "Step 3 — Allowing HTTPS Through the Firewall"
+echo "sudo ufw status"
+echo "sudo ufw allow 'Nginx Full'"
+echo "sudo ufw delete allow 'Nginx HTTP'"
+
+
+# Step 4 — Obtaining an SSL Certificate
+echo "*************************************************"
+echo "Step 4 — Obtaining an SSL Certificate"
+echo "sudo certbot --nginx -d example.com -d www.example.com"
+
+
+#Step 5 — Verifying Certbot Auto-Renewal
+echo "*************************************************"
+echo "Step 5 — Verifying Certbot Auto-Renewal"
+echo "https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-16-04"
+
