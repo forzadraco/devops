93418813609275ae11d8990d0964a319fcb69d83
Ardy Satria Hasanuddin <ardy.hasanuddin@indodeveloper.com>
2017-12-26 21:42:43 +0700

    add curl

--- debian/common/main.sh
+++ debian/common/main.sh
@@ -4,6 +4,8 @@
 sudo apt-get install -y software-properties-common
 sudo apt-get install -y python-software-properties
 sudo apt-get install -y language-pack-en-base
+sudo apt-get install -y curl
+
 
 #git
 sudo add-apt-repository ppa:git-core/ppa
