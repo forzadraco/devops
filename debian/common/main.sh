65507d1649d3a1e198b4bcc98283b02c1ee4c3ce
Ardy Satria Hasanuddin <ardy.hasanuddin@indodeveloper.com>
2017-12-26 21:02:10 +0700

    main install

---
+++ debian/common/main.sh
@@ -0,0 +1,9 @@
+#bin/bash
+
+## common
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y python-software-properties
+sudo apt-get install -y language-pack-en-base
+
+#git
+sudo add-apt-repository ppa:git-core/ppa
