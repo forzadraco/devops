6423db8bd8d0da236933703e64fb21ae5c9d4f78
Ardy Satria Hasanuddin <forzadraco@gmail.com>
2018-02-02 13:29:15 +0700

    Create install-docker-ce-centos-redhat.sh

---
+++ docker/scripts/install-docker-ce-centos-redhat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yum instyall -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce
+systemctl enable docker && systemctl start docker && systemctl status docker
