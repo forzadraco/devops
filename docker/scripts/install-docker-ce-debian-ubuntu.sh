b08973a0cca208f8635a7659673701340b126b3d
Ardy Satria Hasanuddin <forzadraco@gmail.com>
2018-02-08 16:06:01 +0700

    Create install-docker-ce-debian-ubuntu.sh

---
+++ docker/scripts/install-docker-ce-debian-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt install -y docker-ce
+sudo apt update
+sudo systemctl enable docker && sudo systemctl start docker && sudo systemctl status docker
